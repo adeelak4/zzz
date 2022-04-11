d73543fd32b5448a99f6d563b98385b2b7051128
adeelak4 <adeelafzal2015@gmail.com>
2022-04-11 12:31:32 -0700

    adding files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './App.css';
+import Navbar from "./components/Navbar";
+const App = ()=>{
+  return(
+    <>
+    <Navbar title="ADEEL" />
+ <div className="blank">lovely</div>
+ 
+ </>
+  )
+}
+export default App
